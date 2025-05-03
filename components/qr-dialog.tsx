177e9c15c24b0a35de4dab338eccc5999eaa7864
Ankit <kashyap25ankit@gmail.com>
2025-05-04 04:31:09 +0530

    custom rq scanner

--- components/qr-dialog.tsx
+++ components/qr-dialog.tsx
@@ -1,41 +1,51 @@
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { space } from "@/lib/fonts";
-import { QrCode } from "lucide-react";
+"use client";
+
+import { useState } from "react";
+import { motion, AnimatePresence } from "motion/react";
+import { QrCode, X } from "lucide-react";
 import Image from "next/image";
+import { space } from "@/lib/fonts";
 
 export default function QrDialog() {
+  const [showQR, setShowQR] = useState(false);
+
   return (
-    <div className="fixed right-12 bottom-12">
-      <Dialog>
-        <DialogTrigger>
-          {/* <QrCode className="size-12 p-1 rounded-full border cursor-pointer text-primary-btn" /> */}
-          <div className="flex items-center gap-2 py-2 px-4 rounded-full border cursor-pointer  bg-white ">
-            <p className={`${space.className} text-xl  text-primary-btn`}>
-              Scan
-            </p>
-            <QrCode className="text-primary-btn animate-bounce" />
-          </div>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogDescription>
-              <Image
-                src={"/qr.png"}
-                width={300}
-                height={200}
-                alt="whatsapp"
-                className="w-full"
-              />
-            </DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
+    <div className="fixed right-12 bottom-12 flex flex-col items-end z-50">
+      <AnimatePresence>
+        {showQR && (
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 20 }}
+            transition={{ duration: 0.3 }}
+            className="relative mb-3 bg-white p-4 rounded-xl shadow-xl border"
+          >
+            {/* Close Button */}
+            <button
+              onClick={() => setShowQR(false)}
+              className="absolute top-0 cursor-pointer right-0 text-gray-500 hover:text-red-500"
+            >
+              <X size={20} />
+            </button>
+
+            <Image
+              src="/qr.png"
+              width={100}
+              height={100}
+              alt="whatsapp qr"
+              className=""
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+      <div
+        onClick={() => setShowQR(true)}
+        className="flex items-center gap-2 py-2 px-4 rounded-full border cursor-pointer bg-white shadow-xl"
+      >
+        <p className={`${space.className} text-xl text-primary-btn`}>Scan</p>
+        <QrCode className="text-primary-btn animate-bounce" />
+      </div>
     </div>
   );
 }
